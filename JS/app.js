d083733cbac0a86c3e7fba160b9186cdd9ea98af
JoshuaWachana <jjwachana@gmail.com>
2022-03-13 09:46:24 -0700

    javascript code for more navbar responsiveness

--- JS/app.js
+++ JS/app.js
@@ -12,4 +12,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
   about.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
   });
+
+  const skills = document.getElementById('navbar_skills');
+
+  skills.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+  });
+
+  const projects = document.getElementById('navbar_projects');
+
+  projects.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+  });
+
+  const contact = document.getElementById('navbar_contact');
+
+  contact.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+  });
 });
