04ef78bed85df7efb17322933f17190297866664
JoshuaWachana <jjwachana@gmail.com>
2022-03-13 09:28:26 -0700

    hamburger menu now folds back up after a link is clicked in mobile view

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+  //navbar
   const toggleButton = document.getElementsByClassName('toggle-button')[0];
   const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
   toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+  });
+
+  const about = document.getElementById('navbar_about');
+
+  about.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
   });
 });
